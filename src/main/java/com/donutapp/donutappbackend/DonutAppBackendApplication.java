e5239bc87753f33e8a8d1faf26cbbabe017eb891
jessicarodriguezchim <jessica.g.rodriguez.chim@gmail.com>
2025-06-06 11:22:40 -0600

    first commit

---
+++ src/main/java/com/donutapp/donutappbackend/DonutAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.donutapp.donutappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donutapp/donutappbackend/DonutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donutapp.donutappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
